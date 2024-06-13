88b71e2afc4af0143d6d04a3ae63dabfa2258341
akhilbharambe <akhilbharambe444@gmail.com>
2024-06-13 09:04:13 +0530

    updated

--- course-management-api/app.js
+++ course-management-api/app.js
@@ -44,8 +44,10 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 
